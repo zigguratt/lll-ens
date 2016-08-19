e0f9f14bcff12dc20fa82b302bc2431bda53cc93
Daniel Ellison <daniel@syrinx.net>
2016-08-19 07:49:40 -0400

    Removed pseudo-constructor and set root node owner to contract caller.

--- src/ens.sol
+++ src/ens.sol
@@ -1,5 +1,4 @@
 contract ENS {
-    function ENS(address owner);
     function owner(bytes32 node) constant returns(address);
     function resolver(bytes32 node) constant returns(address);
     function setOwner(bytes32 node, bytes32 label, address owner);
