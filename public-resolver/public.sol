37256e07a70401d7fa17b60a66ff06c8041888c8
Daniel Ellison <daniel@syrinx.net>
2016-09-03 12:50:07 -0400

    Added implementation of a public resolver.

---
+++ public-resolver/public.sol
@@ -0,0 +1,8 @@
+contract PublicResolver {
+    function PublicResolver(address ensAddr);
+    function has(bytes32 node, bytes32 kind) constant returns (bool);
+    function addr(bytes32 node) constant returns (address ret);
+    function content(bytes32 node) constant returns (bytes32 ret);
+    function setAddr(bytes32 node, address addr);
+    function setContent(bytes32 node, bytes32 addr);
+}
