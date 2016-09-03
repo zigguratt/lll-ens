269691a31176507da54831aff298e57870f93405
Daniel Ellison <daniel@syrinx.net>
2016-09-03 12:39:12 -0400

    Replaced pseudo-constructor until LLL compiler can handle deployment parameters. Made "only-owner" take an optional node address. Removed incorrect quotes around "records" storage address.

--- ens-registry/ens.sol
+++ ens-registry/ens.sol
@@ -1,4 +1,5 @@
 contract ENS {
+    function ENS(address owner);
     function owner(bytes32 node) constant returns(address);
     function resolver(bytes32 node) constant returns(address);
     function setOwner(bytes32 node, bytes32 label, address owner);
