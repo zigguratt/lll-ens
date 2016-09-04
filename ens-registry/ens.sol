48a5dec4242f07aa8bd67b394ac3fdb9f5fd034c
Daniel Ellison <daniel@syrinx.net>
2016-09-03 20:15:29 -0400

    A bit more cleanup.

--- ens-registry/ens.sol
+++ ens-registry/ens.sol
@@ -2,7 +2,7 @@ contract ENS {
     function ENS(address owner);
     function owner(bytes32 node) constant returns(address);
     function resolver(bytes32 node) constant returns(address);
-    function setOwner(bytes32 node, bytes32 label, address owner);
     function setOwner(bytes32 node, address owner);
+    function setSubnodeOwner(bytes32 node, bytes32 label, address owner);
     function setResolver(bytes32 node, address resolver);
 }
