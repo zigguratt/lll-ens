9b65aa4a15c69b3c82c5ee801de8e9a9c74c8b44
Daniel Ellison <daniel@syrinx.net>
2016-09-29 14:36:36 -0400

    Added TTL support.

--- ens-registry/ens.sol
+++ ens-registry/ens.sol
@@ -2,7 +2,9 @@ contract ENS {
     function ENS(address owner);
     function owner(bytes32 node) constant returns(address);
     function resolver(bytes32 node) constant returns(address);
+    function ttl(bytes32 node) constant returns(uint64);
     function setOwner(bytes32 node, address owner);
     function setSubnodeOwner(bytes32 node, bytes32 label, address owner);
     function setResolver(bytes32 node, address resolver);
+    function setTTL(bytes32 node, uint64 ttl);
 }
