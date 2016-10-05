a4e4481db509872bb5fc4073b412ece7e732e31e
Daniel Ellison <daniel@syrinx.net>
2016-10-05 07:32:26 -0400

    Added a simple and obvious comment for consistency.

--- public-resolver/public.sol
+++ public-resolver/public.sol
@@ -1,4 +1,6 @@
 contract PublicResolver {
+
+    // Functions.
     function PublicResolver(address ensAddr);
     function has(bytes32 node, bytes32 kind) constant returns (bool);
     function addr(bytes32 node) constant returns (address ret);
