49a99ee8efefd3723457b8333de2d3adb646d1ae
Daniel Ellison <daniel@syrinx.net>
2016-11-30 13:59:24 -0500

    Modified to match newer version of PublicResolver.sol.

--- public-resolver/public.sol
+++ public-resolver/public.sol
@@ -3,8 +3,9 @@ contract PublicResolver {
     // Functions.
     function PublicResolver(address ensAddr);
     function has(bytes32 node, bytes32 kind) constant returns (bool);
+    function supportsInterface(bytes4 interfaceID) constant returns (bool);
     function addr(bytes32 node) constant returns (address ret);
-    function content(bytes32 node) constant returns (bytes32 ret);
     function setAddr(bytes32 node, address addr);
-    function setContent(bytes32 node, bytes32 addr);
+    function hash(bytes32 node) constant returns (bytes32 ret);
+    function setHash(bytes32 node, bytes32 hash);
 }
